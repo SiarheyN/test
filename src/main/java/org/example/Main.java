aef08399dc56aa2d35c0638d2f46de8ade7ee4bc
i.geylash <iryna.heilash@gmail.com>
2023-03-19 18:24:55 +0300

    commit from ira

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 public class Main {
     public static void main(String[] args) {
         try {
+            System.out.println("Hello from ira");
             Class.forName("org.postgresql.Driver");
             Connection con = DriverManager.getConnection(
                     "jdbc:postgresql://localhost:5432/test", "postgres", null);
