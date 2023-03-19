290213fa29c19a67f0848c1d1ac996f68b4d6c9a
siarhei <novik.sergey@mail.ru>
2023-03-19 18:06:02 +0300

    1 commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/test", "postgres", null);
+//here sonoo is database name, root is username and password
+            Statement stmt = con.createStatement();
+//,mn,mn
+//            xcvzcvzxc
+
+            String query = "CREATE TABLE public.test_table1 (\n" +
+                    "\tcolumn_test varchar NULL,\n" +
+                    "\tcol2 varchar NULL\n" +
+                    ")";
+//            ResultSet rs = stmt.executeQuery(query);
+            ResultSet rs = stmt.executeQuery("select * from public.test_table1");
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + "  " + rs.getString(2));
+            }
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
